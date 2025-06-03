030bb60d7268130b800455cbd40d47e8c52887e9
Trung DT <thanhtrung.it25@gmail.com>
2025-06-03 16:32:38 +0700

    Add ratelimit

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -5,11 +5,19 @@ import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
+import { headers } from "next/headers";
+import ratelimit from "../ratelimit";
+import { redirect } from "next/navigation";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
 ) => {
   const { email, password } = params;
+
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) redirect("/too-fast");
 
   try {
     const result = await signIn("credentials", {
@@ -31,6 +39,11 @@ export const signInWithCredentials = async (
 
 export const signUp = async (params: AuthCredentials) => {
   const { email, fullName, password, universityCard, universityId } = params;
+
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) redirect("/too-fast");
 
   const existingUser = await db
     .select()
