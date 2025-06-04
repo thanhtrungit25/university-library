4546416d28033212c1bb538fa990262783f7d979
Trung DT <thanhtrung.it25@gmail.com>
2025-06-04 16:38:30 +0700

    Fix check

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -31,30 +31,28 @@ const BookOverview = async ({
   .where(eq(users.id, userId))
   .limit(1);
 
-  if (!user) return null;
-
   const borrowingEligibility = {
-    isEligible: availableCopies > 0 && user.status === "APPROVED",
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
     message: availableCopies <= 0 ? "Book is not available" : "You are not eligible to borrow this book"
   }
 
   return (
-    <section className='book-overview'>
+    <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
         <h1>{title}</h1>
 
         <div className="book-info">
-          <p className='font-semibold text-light-200'>
+          <p className="font-semibold text-light-200">
             By <span>{author}</span>
           </p>
 
           <p>
             Category:{" "}
-            <span className='font-semibold text-lime-200'>{genre}</span>
+            <span className="font-semibold text-lime-200">{genre}</span>
           </p>
 
           <div className="flex flex-row gap-1">
-            <Image src="/icons/star.svg" alt='star' width={22} height={22} />
+            <Image src="/icons/star.svg" alt="star" width={22} height={22} />
             <p>{rating}</p>
           </div>
         </div>
@@ -62,15 +60,19 @@ const BookOverview = async ({
         <div className="book-copies">
           <p>
             Total Books <span>{totalCopies}</span>
-          </p> 
+          </p>
           <p>
             Available Books <span>{availableCopies}</span>
-          </p> 
+          </p>
         </div>
 
         <div className="book-description">{description}</div>
 
-        <BorrowBook bookId={id} userId={userId} borrowingEligibility={borrowingEligibility} />
+        {user && <BorrowBook
+          bookId={id}
+          userId={userId}
+          borrowingEligibility={borrowingEligibility}
+        />}
       </div>
 
       <div className="relative flex flex-1 justify-center">
@@ -82,7 +84,7 @@ const BookOverview = async ({
             coverImage={coverUrl}
           />
 
-          <div className='absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden'>
+          <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
             <BookCover
               variant="wide"
               coverColor={coverColor}
@@ -92,7 +94,7 @@ const BookOverview = async ({
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default BookOverview
